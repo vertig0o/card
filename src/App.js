d32f7a347567f595a48da3efdc36f13baddd6f9f
verti0o <atilay_yoruk@outlook.com>
2023-05-04 17:05:21 +0300

    Card style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "bulma/css/bulma.css";
+import Course from "./Course";
+import "./App.css";
+import Angular from "./images/angular.jpg";
+import Bootstrap from "./images/boostrap.png";
+import Csharp from "./images/ccsharp.png";
+import Kompleweb from "./images/kompleweb.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="hero is-link">
+        <div className="hero-body">
+          <p className="title">Kurslarım</p>
+        </div>
+      </section>
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column">
+              <Course
+                image={Angular}
+                title="Angular"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptate amet facilis incidunt at fugiat ducimus totam ad rerum? Blanditiis
+              voluptatibus nemo et vitae facilis nesciunt,quaerat repellat porro aliquam. Pariatur. "
+              />
+            </div>
+            <div className="column">
+              <Course
+                image={Bootstrap}
+                title="Bootstrap"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptate amet facilis incidunt at fugiat ducimus totam ad rerum? Blanditiis
+              voluptatibus nemo et vitae facilis nesciunt,quaerat repellat porro aliquam. Pariatur. "
+              />
+            </div>
+            <div className="column">
+              <Course
+                image={Csharp}
+                title="Csharp Web"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptate amet facilis incidunt at fugiat ducimus totam ad rerum? Blanditiis
+              voluptatibus nemo et vitae facilis nesciunt,quaerat repellat porro aliquam. Pariatur. "
+              />
+            </div>
+            <div className="column">
+              <Course
+                image={Kompleweb}
+                title="Frontend"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+               Voluptate amet facilis incidunt at fugiat ducimus totam ad rerum? Blanditiis
+                voluptatibus nemo et vitae facilis nesciunt,quaerat repellat porro aliquam. Pariatur. "
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
